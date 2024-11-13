c01b07cbccfff7f6fa6f0dbe7b28092dc972c220
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-11-13 22:03:00 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,5 +1,6 @@
 # 此脚本用处是：添加第三方插件
-#===============================================================
+#=========================================================================================
+
 
 # 1-添加 ShadowSocksR Plus+ 插件
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -8,4 +9,5 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
