0aa3cc99e71a8c52bb7614162b10b29bc5424229
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-11-13 22:15:18 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,3 +1,6 @@
 rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-openclash
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
