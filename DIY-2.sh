b1dbc7e6e1378e1b40f0aa84f4d7f358c1856cbd
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-11-14 00:03:23 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,4 +1,2 @@
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-openclash
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
